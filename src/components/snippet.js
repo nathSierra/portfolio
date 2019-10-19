da41a7ef12a54c6a96b31ad86cafb00abb7fa743
Nathan Sierra <arreisnahtan@gmail.com>
2019-10-19 01:10:59 -0400

    making the website more readable and mobile-friendly by adding less restricting styles and making font larger

--- src/components/snippet.js
+++ src/components/snippet.js
@@ -3,13 +3,13 @@ import Colors from '../constants/Colors';
 
 const Snippet = Styled.div`
   flex-grow: 1;
-  min-width: 350px;
   border-radius: 2px;
   border-width: 1px;
   border-style: solid;
   border-color: black;
   background: ${Colors.backgroundDarkest};
   color: ${props => props.inputColor || 'white'};
+  font-size: 24px;
   font-family: 'Inconsolata', monospace;
   margin-bottom: 10vh;
   padding: 0.5vw;
