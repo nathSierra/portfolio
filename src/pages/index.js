d17497b4951e5712204ceede2c1d20f01c4007c0
Nathan Sierra <arreisnahtan@gmail.com>
2019-10-19 00:37:30 -0400

    adds more words

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,31 +12,36 @@ import Colors from '../constants/Colors'
 const IndexPage = (location) => (
   <Layout location={location}>
     <SEO title="Home" />
-    <h1>hello.</h1>
+    <h2>hello.</h2>
 
     <Snippet>
       <p>My name is Nathan Sierra.</p>
       <p>I'm a graduate of Southern Illinois University. I was an English student.</p>
       <p>Now I learn about computers, talk to computers, and talk about computers.</p>
       <p>I am a freelance, fullstack web developer. <Highlight inputColor={Colors.primary}>// a self-taught > bootcamp > self-taught sequence</Highlight></p>
-      <p>However, I specialize in front-end, React.js development.</p>
+      <p>However, I specialize in front-end, React.js development. <Highlight inputColor={Colors.primary}> // this doesn't mean I'm against Angular-- I see the benefits.</Highlight></p>
       <p>I also work part time as a tutor and teaching assistant.</p>
       <p>I'm currently learning and practicing React Native. I should soon have a few projects out on the App Store. <Highlight inputColor={Colors.primary}>// fingers crossed</Highlight></p>
     </Snippet>
     <h2 style={{color: Colors.purple}}>why?</h2>
     <br></br>
     <Snippet inputColor={Colors.purple}>
-      <p>At first I was overwhelmed by the deluge of comp sci info.</p>
+      <p>English is a passion of mine. However, I didn't want to be stuck in academia all my life.</p>
+      <p>Why programming?</p>
+      <p>Well.</p>
+      <p>It's tough.</p>
+      <p>At first I was overwhelmed by the deluge of comp sci info.<Highlight inputColor={Colors.primary}> // I thought I couldn't possibly learn it.</Highlight></p>
       <p>My mindset has changed drastically.</p>
+      <p>I don't want to be deny myself the opportunity to become a software developer purely due to fear.</p>
       <p>Now I'm glad I have the opportunity to use what I've learned to create products and solve problems.</p>
       <p>I'm dedicating myself to learning something new in software everyday.
-      <Highlight inputColor={Colors.primary}>// to be frank, I don't see that ever stopping.</Highlight></p>
+      <Highlight inputColor={Colors.primary}> // to be frank, I don't see that ever stopping.</Highlight></p>
       
     </Snippet>
     <h2 style={{color: Colors.seafoam}}>contact</h2>
     <Snippet inputColor={Colors.seafoam}>
-      <p>If you wanna talk about a project or nerd out about tech, don't hesitate to reach out!</p>
-      <p>Contact me at <a href='mailto:arreisnahtan@gmail.com' style={{color: `${Colors.purple}`}}>arreisnahtan@gmail.com</a> </p>
+      <p>If you wanna talk about a project or nerd out about tech, coffee, literature, or beer don't hesitate to reach out!</p>
+      <p>Contact me at <a href='mailto:arreisnahtan@gmail.com' style={{color: `${Colors.purple}`}}>arreisnahtan@gmail.com</a> <Highlight inputColor={Colors.primary}> //Let's see if we can craft you a solution.</Highlight></p>
       <br></br>
       <p>Technology has the power to transform the world for the better; we have the responsibility to make it so.</p>
       </Snippet>
